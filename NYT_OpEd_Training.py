ecfb9fd375dbdf73ac99adf9116a62c9c8ce30b4
Samuel A. Jocas <69360783+sjocas14@users.noreply.github.com>
2021-02-11 11:54:20 -0500

    Scikit Training Script

---
+++ NYT_OpEd_Training.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 11 10:10:01 2021
+
+@author: samueljocas
+"""
+
+# import necessary libraries
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import GaussianNB
+import pandas as pd
+import pickle
+
+filepath = "/Users/samueljocas/Desktop/Data Science Portfolio/New York Times ML Algorithm  /Op-Ed Training Datasets/Training-Dataset-2021-02-10 21:13:43.876754.csv"
+data = pd.read_csv(filepath, encoding = "ISO-8859-1")
+
+X = data.iloc[:,4] # extract column with news article body
+
+print(X)
+
+vectorizer = CountVectorizer(stop_words='english')
+X_vec = vectorizer.fit_transform(X.values.astype('U'))
+pickle.dump(vectorizer, open("vectorizer_NYT", 'wb')) # Save vectorizer for reuse
+X_vec = X_vec.todense()
+
+tfidf = TfidfTransformer() #by default applies "l2" normalization
+X_tfidf = tfidf.fit_transform(X_vec)
+X_tfidf = X_tfidf.todense()
+
+
+##################Apply Naive Bayes algorithm to train data####################
+# Extract the news body and labels for training the classifier
+
+X_train = X_tfidf[:,:]
+Y_train = data.iloc[:,3]
+
+# Train the NB classifier
+clf = GaussianNB().fit(X_train, Y_train) 
+pickle.dump(clf, open("nb_clf_NYT", 'wb')) # Save classifier for reuse
